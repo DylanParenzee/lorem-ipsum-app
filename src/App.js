e4ee642bedcc9c2b5fe03633d3984efe09c8a43b
DylanParenzee <dylanparenzee@hotmail.com>
2022-09-13 06:29:02 +1000

    added basic visual for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submited");
+  };
+
   return (
-    <>
-      <div>
-        <h1>hello</h1>
-      </div>
-    </>
+    <section className="section-center">
+      <h3>Tired of boring Lorem-Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs :</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+          sapiente cumque, doloremque incidunt officiis possimus iure placeat
+          labore corrupti, tenetur consectetur dolorum. Laborum tempore expedita
+          quae molestias dolores minima vitae!
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+          sapiente cumque, doloremque incidunt officiis possimus iure placeat
+          labore corrupti, tenetur consectetur dolorum. Laborum tempore expedita
+          quae molestias dolores minima vitae!
+        </p>
+      </article>
+    </section>
   );
 };
 
